43ce205e9d57f68564cd3d2b4490d8056156db65
Mabilog <g.angelo.m@gmail.com>
2021-09-06 20:59:33 -0400

    github pages deployed!

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -33,12 +33,9 @@ const FavoriteWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  /* bottom: 0; */
-  width: 100vw;
-  height: 100vh;
   background-color: aliceblue;
-  /* overflow-x: auto; */
-
+  width: 100%;
+  height: 100vh;
   
 `;
 
